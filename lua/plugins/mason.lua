7f15cd0eeff20e902191c6f2ba930372534a4727
IHA Yasushi <yasuc18@gmail.com>
2024-11-07 20:44:35 +0900

    feat: 🎸 add sixelview.lua

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+if false then
+  return {}
+end
+return {
+  "williamboman/mason.nvim",
+  cmd = {
+    "Mason",
+    "MasonInstall",
+    "MasonUninstall",
+    "MasonUninstallAll",
+    "MasonUpdate",
+    "MasonLog",
+  },
+}
---
+++ lua/plugins/sixelview.lua
@@ -0,0 +1,15 @@
+if false then
+  return {}
+end
+return {
+  "kjuq/sixelview.nvim",
+  opts = {
+    -- a table to specify what files should be viewed by this plugin
+    pattern = {},
+    -- whether to show an image automatically when an image buffer is opened
+    auto = true,
+    -- time of delay before showing image
+    -- try setting this duration longer if you have a trouble showing image
+    delay_ms = 100,
+  },
+}
