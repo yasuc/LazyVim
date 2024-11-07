0a7977d895b9360b4467d65dc50485a45b6dbcf4
IHA Yasushi <yasuc18@gmail.com>
2024-11-07 15:49:36 +0900

    feat: 🎸 add codeium.lua

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,22 @@
+return {
+  "Exafunction/codeium.vim",
+  cmd = {
+    "Codeium",
+    "CodeiumEnable",
+    "CodeiumDisable",
+    "CodeiumToggle",
+    "CodeiumAuto",
+    "CodeiumManual",
+  },
+  event = "BufEnter",
+  keys = {
+    -- suggested keymap
+    { ",p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+
+    { "<Leader>;", "<Cmd>CodeiumToggle<CR>", noremap = true, desc = "Toggle Codeium active", mode="n" },
+    { "<C-g>", function() return vim.fn["codeium#Accept"]() end, expr = true, mode="i"},
+    { "<C-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, expr = true, mode="i" },
+    { "<C-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, mode="i" },
+    { "<C-x>", function() return vim.fn["codeium#Clear"]() end, expr = true, mode="i" },
+  },
+}
