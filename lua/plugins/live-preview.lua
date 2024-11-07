6ed91735d9ffbbb4ae97879f82639444383de06a
IHA Yasushi <yasuc18@gmail.com>
2024-11-07 14:34:57 +0900

    feat: 🎸 install rest-nvim & browser, img-clip, live-preview

---
+++ lua/plugins/live-preview.lua
@@ -0,0 +1,9 @@
+if false then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    "brianhuster/autosave.nvim", -- Not required, but recomended for autosaving and sync scrolling
+    "nvim-telescope/telescope.nvim", -- Not required, but recommended for integrating with Telescope
+  },
+  opts = {},
+}
