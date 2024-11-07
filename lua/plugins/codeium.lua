0fa1618177a1389c221e8404840498eb1fc874c7
IHA Yasushi <yasuc18@gmail.com>
2024-11-07 16:04:49 +0900

    fix: 🐛 modify codeium key bind

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -15,8 +15,8 @@ return {
 
     { "<Leader>;", "<Cmd>CodeiumToggle<CR>", noremap = true, desc = "Toggle Codeium active", mode="n" },
     { "<C-g>", function() return vim.fn["codeium#Accept"]() end, expr = true, mode="i"},
-    { "<C-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, expr = true, mode="i" },
-    { "<C-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, mode="i" },
+    { "<C-u>", function() return vim.fn["codeium#CycleCompletions"](1) end, expr = true, mode="i" },
+    { "<C-d>", function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, mode="i" },
     { "<C-x>", function() return vim.fn["codeium#Clear"]() end, expr = true, mode="i" },
-  },
+},
 }
