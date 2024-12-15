9dd93d6397bb29bff71fe2c93c46c3c435939324
IHA Yasushi <yasuc18@gmail.com>
2024-12-15 22:21:52 +0900

    feat: 🎸 add skkeleton.lua

---
+++ lua/plugins/skkeleton.lua
@@ -0,0 +1,36 @@
+if false then
+  return {}
+end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  "vim-skk/skkeleton",
+  keys = {
+    { "<C-j>", "<Plug>(skkeleton-enable)", noremap = false, mode = "i" },
+    { "<C-j>", "<Plug>(skkeleton-enable)", noremap = false, mode = "c" },
+  },
+  dependencies = {
+    "vim-denops/denops.vim",
+    -- "Shougo/ddc.vim",
+    -- "Shougo/ddc-ui-native",
+  },
+  config = function()
+    vim.fn["skkeleton#config"]({
+      globalDictionaries = { "~/.config/eskk/SKK-JISYO.L" },
+      eggLikeNewline = true,
+    })
+    -- vim.fn["ddc#custom#patch_global"]("ui", "native")
+    -- vim.fn["ddc#custom#patch_global"]("sources", { "skkeleton" })
+    -- vim.fn["ddc#custom#patch_global"]({
+    --   sourceOptions = {
+    --     ["skkeleton"] = {
+    --       mark = "skkeleton",
+    --       matchers = {},
+    --       sorters = {},
+    --       converters = {},
+    --       isVolatile = true,
+    --       minAutoCompleteLength = 1,
+    --     },
+    --   },
+    -- })
+    -- vim.fn["ddc#enable"]()
+  end,
+}
